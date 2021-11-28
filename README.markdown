466f02b3d2ef6447939837c14e84a35cd30dfce8
DanOLondon <45563351+DanOLondon@users.noreply.github.com>
2021-11-28 10:30:36 +0000

    Add files via upload

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Survival Predictor
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/danielo1992/pen/VwMZWrd](https://codepen.io/danielo1992/pen/VwMZWrd).
+
+
---
+++ dist/script.js
@@ -0,0 +1,22 @@
+//the max is 18 days as that is what I did
+var days = {
+  totalDays: 18,
+  generate: function () {
+    var randomNumber = Math.floor(Math.random() * this.totalDays) + 1;
+    return randomNumber;
+  }
+}
+
+//Reveals how many days you will survive
+
+function showNumber(number) {
+  var placeholder = document.getElementById('placeholder');
+  placeholder.innerHTML = number;
+}
+
+var button = document.getElementById('button');
+
+button.onclick = function() {
+  var result = days.generate();
+  showNumber(result);
+};
